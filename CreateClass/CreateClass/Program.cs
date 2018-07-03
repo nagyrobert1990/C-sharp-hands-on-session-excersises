dd61ab537c7f4e4113b08e95b33a903d5cbc07f5
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-07-03 14:21:39 +0200

    2nd excersise completed

--- CreateClass/CreateClass/Program.cs
+++ CreateClass/CreateClass/Program.cs
@@ -4,37 +4,73 @@ namespace CreateClass
 {
     public enum Genders { Male, Female };
 
-    class Person
+    public class Person
     {
-        public string Name;
-        public DateTime BirthDate;
-        public Genders Gender;
+        public string name;
+        public DateTime birthDate;
+        public Genders gender;
 
         public Person(string name, DateTime birthDate, Genders gender)
         {
-            Name = name;
-            BirthDate = birthDate;
-            Gender = gender;
+            this.name = name;
+            this.birthDate = birthDate;
+            this.gender = gender;
         }
 
         public override string ToString()
         {
-            string Result = "";
-            return Result
-                + Name + " "
-                + BirthDate.Year + ". "
-                + BirthDate.Month + ". "
-                + BirthDate.Day + ". "
-                + Gender;
+            return $"{name}"
+                + $" {birthDate.Year}"
+                + $" {birthDate.Month}"
+                + $" {birthDate.Day}"
+                + $" {gender}";
+        }
+    }
+
+    public class Room
+    {
+        public int roomNumber;
+
+        public Room(int roomNumber)
+        {
+            this.roomNumber = roomNumber;
         }
 
+        public override string ToString()
+        {
+            return $"{roomNumber}";
+        }
     }
+
+    public class Employee : Person
+    {
+        int salary;
+        string profession;
+        Room room;
+
+        public Employee(string name, DateTime birthDate, Genders gender, int salary, string profession, Room room)
+            : base(name, birthDate, gender)
+        {
+            this.salary = salary;
+            this.profession = profession;
+            this.room = room;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" {salary}" + $" {profession}" + $" {room}";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Person Jack = new Person("Jack", new DateTime(1989, 12, 11), Genders.Male);
+            Room room = new Room(12);
+            Person Joe = new Employee("Joe", new DateTime(1977, 09, 01), Genders.Female, 50000, "assistant", room);
             Console.WriteLine(Jack.ToString());
+            Console.WriteLine(Joe.ToString());
         }
     }
 }

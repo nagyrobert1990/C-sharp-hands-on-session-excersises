bc52e9fb7547cff978afa0e091a35e8784f46679
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-07-03 14:57:53 +0200

    3rd excersise corrected

--- CreateClass/CreateClass/Program.cs
+++ CreateClass/CreateClass/Program.cs
@@ -52,24 +52,36 @@ namespace CreateClass
 
     #region Employee class
 
-    public class Employee : Person
+    public class Employee : Person, ICloneable
     {
-        int salary;
-        string profession;
-        Room room;
+        public int salary;
+        public string profession;
+        public Room room;
 
-        public Employee(string name, DateTime birthDate, Genders gender, int salary, string profession, Room room)
+        public Employee(string name, DateTime birthDate, Genders gender, int salary, string profession)
             : base(name, birthDate, gender)
         {
             this.salary = salary;
             this.profession = profession;
-            this.room = room;
         }
 
         public override string ToString()
         {
             return base.ToString() + $" {salary}" + $" {profession}" + $" {room}";
         }
+
+        object ICloneable.Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public object Clone()
+        {
+            Employee newEmployee = (Employee)this.MemberwiseClone();
+            newEmployee.room = new Room(this.room.roomNumber);
+            return newEmployee;
+        }
+
     }
 
     #endregion
@@ -78,11 +90,13 @@ namespace CreateClass
     {
         static void Main(string[] args)
         {
-            Person Jack = new Person("Jack", new DateTime(1989, 12, 11), Genders.Male);
-            Room room = new Room(12);
-            Person Joe = new Employee("Joe", new DateTime(1977, 09, 01), Genders.Female, 50000, "assistant", room);
-            Console.WriteLine(Jack.ToString());
-            Console.WriteLine(Joe.ToString());
+            Employee Kovacs = new Employee("Géza", DateTime.Now, Genders.Male, 1000, "léhűtő");
+            Kovacs.room = new Room(111);
+            Employee Kovacs2 = (Employee)Kovacs.Clone();
+            Kovacs2.room.roomNumber = 112;
+            Console.WriteLine(Kovacs.ToString());
+            Console.WriteLine(Kovacs2.ToString());
+            Console.ReadKey();
         }
     }
 }

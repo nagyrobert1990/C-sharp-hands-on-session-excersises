b93f96ef4bc4e31a5a61a68c2a8e97eb7b3fb1da
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-07-03 13:22:49 +0200

    1st excersise is ready

---
+++ CreateClass/CreateClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CreateClass
+{
+    public enum Genders { Male, Female };
+
+    class Person
+    {
+        public string Name;
+        public DateTime BirthDate;
+        public Genders Gender;
+
+        public Person(string name, DateTime birthDate, Genders gender)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            string Result = "";
+            return Result
+                + Name + " "
+                + BirthDate.Year + ". "
+                + BirthDate.Month + ". "
+                + BirthDate.Day + ". "
+                + Gender;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Jack = new Person("Jack", new DateTime(1989, 12, 11), Genders.Male);
+            Console.WriteLine(Jack.ToString());
+        }
+    }
+}
